575b07a6d75aca1956e36c9b7b2949ed7562f3e4
AngelMD55 <angel_demerutis@hotmail.com>
2019-03-18 08:32:11 -0700

    Updates

---
+++ precache-manifest.a2b9a3323b091f41423acf22777273cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "4c49cb1505e12b9ba640",
+    "url": "/Clicky-Game/static/js/main.7490ecc2.chunk.js"
+  },
+  {
+    "revision": "8f6f18fb074621dfc5ee",
+    "url": "/Clicky-Game/static/js/2.fadd7c56.chunk.js"
+  },
+  {
+    "revision": "4c49cb1505e12b9ba640",
+    "url": "/Clicky-Game/static/css/main.11a30fb5.chunk.css"
+  },
+  {
+    "revision": "a71b3c7394a498885919be019fe662de",
+    "url": "/Clicky-Game/index.html"
+  }
+];
